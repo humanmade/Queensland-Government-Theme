ffd01e565eb5d1f67b0344fbf2810222e9b701d9
Bronson Quick <bronson@sennza.com.au>
2015-07-02 12:22:21 +0200

    Change text domain to qldgovt for now

--- parts/content.php
+++ parts/content.php
@@ -6,11 +6,11 @@
 
 			<div class="page-options" id="pre-page-options">
 				<ul>
-					<li><a href="#"><?php esc_html_e( 'Share' ,'hmcue' ); ?></a></li>
-					<li><a href="<?php echo esc_url( get_post_comments_feed_link() ); ?>"><?php esc_html_e( 'Subscribe' ,'hmcue' ); ?></a>
+					<li><a href="#"><?php esc_html_e( 'Share' ,'qldgovt' ); ?></a></li>
+					<li><a href="<?php echo esc_url( get_post_comments_feed_link() ); ?>"><?php esc_html_e( 'Subscribe' ,'qldgovt' ); ?></a>
 						<ul>
-							<li><a href="<?php echo esc_url( get_post_comments_feed_link() ); ?>"><?php esc_html_e( 'RSS feed', 'hmcue' ); ?></a></li>
-							<li><a href="<?php echo esc_url( get_post_comments_feed_link( null, 'atom' ) ); ?>"><?php esc_html_e( 'Atom feed', 'hmcue' ); ?></a></li>
+							<li><a href="<?php echo esc_url( get_post_comments_feed_link() ); ?>"><?php esc_html_e( 'RSS feed', 'qldgovt' ); ?></a></li>
+							<li><a href="<?php echo esc_url( get_post_comments_feed_link( null, 'atom' ) ); ?>"><?php esc_html_e( 'Atom feed', 'qldgovt' ); ?></a></li>
 							<!--<li><a href="#">Email newsletter</a></li>-->
 						</ul>
 					</li>
--- parts/properties.php
+++ parts/properties.php
@@ -1,7 +1,7 @@
 <div id="document-properties">
 	<div class="box-sizing">
 		<dl>
-			<dt><?php esc_html_e( 'Last updated', 'hmcue' ); ?></dt>
+			<dt><?php esc_html_e( 'Last updated', 'qldgovt' ); ?></dt>
 			<dd><?php the_modified_date(); ?></dd>
 		</dl>
 	</div>
