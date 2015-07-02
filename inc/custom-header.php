9e1bb95b33c23b7a5488a5403f251c94e272760f
Tarei King <tarei@humanmade.co.uk>
2015-07-02 10:31:37 +0200

    Add fallback custom header logo

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -4,7 +4,7 @@
  */
 function qldgovt_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'qldgovt_custom_header_args', array(
-		'default-image'          => '',
+		'default-image'          => get_template_directory_uri() . '/assets/images/qg-coa.png',
 		'default-text-color'     => '000000',
 		'width'                  => 287,
 		'height'                 => 50,
