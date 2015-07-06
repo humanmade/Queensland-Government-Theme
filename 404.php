926cab3fa808da802447fbb924f3d12467b5a8dc
Bronson Quick <bronson@sennza.com.au>
2015-07-07 09:11:56 +1000

    Add additional escaping

--- 404.php
+++ 404.php
@@ -15,15 +15,15 @@ get_header(); ?>
 						<h1><?php esc_html_e( 'The page cannot be found', 'hmcue' ); ?></h1>
 
 						<div class="section">
-							<p><?php _e( 'You can:', 'hmcue' ); ?></p>
+							<p><?php esc_html_e( 'You can:', 'hmcue' ); ?></p>
 							<ul>
 								<li><?php
 									printf( __( 'Return to the <a href="%s">home page</a> and look for links to the information;', 'hmcue' ),
 										esc_url( home_url() )
 									);
 								?></li>
-								<li><?php _e( 'Use the search functionality at the top of the page to search across the site; or', 'hmcue' ); ?></li>
-								<li><?php _e( 'Double check the address.', 'hmcue' ); ?></li>
+								<li><?php esc_html_e( 'Use the search functionality at the top of the page to search across the site; or', 'hmcue' ); ?></li>
+								<li><?php esc_html_e( 'Double check the address.', 'hmcue' ); ?></li>
 							</ul>
 						</div><!-- .section -->
 
