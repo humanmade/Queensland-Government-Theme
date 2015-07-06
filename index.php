8edbb6bc0743cdb9cc63d1ad8753812ba6250160
Bronson Quick <bronson@sennza.com.au>
2015-07-07 09:43:41 +1000

    Add a file doc to index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ */
+
+get_header(); ?>
 
 <div id="page-container"><div class="max-width">
 
