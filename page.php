9e513a58a49ada72f0cc32e3b97edafc84417669
Bronson Quick <bronson@sennza.com.au>
2015-07-07 09:48:05 +1000

    Add another file doc

--- page.php
+++ page.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ */
 
 get_header();
 the_post();
