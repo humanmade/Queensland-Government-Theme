2a2129da4ad7f3cf256ae1fadb2c0c0f4e5e466a
Bronson Quick <bronson@sennza.com.au>
2015-07-07 09:17:34 +1000

    Add a blank line after the file doc

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 /**
  * Our super wonderful Header
  */
+
 $suffix = SCRIPT_DEBUG ? '' : '.min';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
